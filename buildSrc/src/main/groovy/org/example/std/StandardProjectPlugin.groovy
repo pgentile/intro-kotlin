8d353cc0221352d373d14d527086b4fb3641c91a
Pierre Gentile <pierre.gentile.perso@gmail.com>
2018-03-21 22:04:47 +0100

    Initial commit

---
+++ buildSrc/src/main/groovy/org/example/std/StandardProjectPlugin.groovy
@@ -0,0 +1,87 @@
+package org.example.std
+
+import com.github.benmanes.gradle.versions.VersionsPlugin
+import org.example.codequality.CodeQualityPlugin
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.artifacts.ComponentSelection
+import org.gradle.api.plugins.JavaPlugin
+import org.gradle.plugins.ide.idea.IdeaPlugin
+
+class StandardProjectPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+        StandardProjectExtension ext = project.extensions.create('standardProject', StandardProjectExtension)
+
+        project.plugins.with {
+            apply(JavaPlugin)
+            apply('kotlin') // Il n'aime pas quand on met la classe en direct
+            apply(IdeaPlugin)
+            apply(VersionsPlugin)
+            apply(CodeQualityPlugin)
+        }
+
+        configureJava(project)
+
+        project.afterEvaluate {
+            configureJUnit(project, ext)
+            configureDependencyUpdates(project)
+        }
+    }
+
+    /**
+     * Configure Java project properties.
+     */
+    private void configureJava(Project project) {
+        project.sourceCompatibility = 1.8
+        project.targetCompatibility = project.sourceCompatibility
+    }
+
+    private void configureKotlin(Project project) {
+        compile "org.jetbrains.kotlin:kotlin-stdlib"
+        compile "org.jetbrains.kotlin:kotlin-stdlib-jdk8"
+        compile "org.jetbrains.kotlin:kotlin-reflect"
+    }
+
+    /**
+     * Use JUnit 5.
+     */
+    private void configureJUnit(Project project, StandardProjectExtension ext) {
+        project.test {
+            useJUnitPlatform {
+                includeEngines 'junit-jupiter'
+            }
+        }
+
+        project.dependencies {
+            testCompile group: 'org.junit.jupiter', name: 'junit-jupiter-api', version: ext.junitVersion
+            testRuntime group: 'org.junit.jupiter', name: 'junit-jupiter-engine', version: ext.junitVersion
+        }
+    }
+
+    /**
+     * Only check versions with released packages.
+     */
+    private void configureDependencyUpdates(Project project) {
+        project.dependencyUpdates {
+
+            resolutionStrategy = {
+
+                componentSelection { rules ->
+                    rules.all { ComponentSelection selection ->
+                        boolean rejected = ['alpha', 'beta', 'rc', 'cr', 'm'].any { qualifier ->
+                            selection.candidate.version ==~ /(?i).*[.-]${qualifier}[.\d-]*/
+                        }
+                        if (rejected) {
+                            selection.reject('Release candidate')
+                        }
+                    }
+                }
+
+            }
+
+        }
+    }
+
+}
